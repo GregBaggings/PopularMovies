0d1b6ff428137caaf152b2d21b971bd2767a0db2
Gergely Agnecz <agnegerg@gmail.com>
2018-03-31 14:32:03 +0200

    Added trailers section with embedded youtube player prototype

--- app/src/main/java/io/git/movies/popularmovies/activities/DetailsActivity.java
+++ app/src/main/java/io/git/movies/popularmovies/activities/DetailsActivity.java
@@ -7,15 +7,22 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.git.movies.popularmovies.BuildConfig;
 import io.git.movies.popularmovies.R;
 import io.git.movies.popularmovies.pojos.MovieDetails;
 import io.git.movies.popularmovies.utils.URLBuilder;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements YouTubePlayer.OnInitializedListener {
     @BindView(R.id.titleTV)
     TextView titleTV;
     @BindView(R.id.plotTV)
@@ -27,6 +34,10 @@ public class DetailsActivity extends AppCompatActivity {
     @BindView(R.id.posterIV)
     ImageView posterIV;
 
+    private YouTubePlayerFragment playerFragment;
+    private YouTubePlayer mPlayer;
+    private String YouTubeKey = BuildConfig.YoutubeAPIKey;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +47,11 @@ public class DetailsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         populateMovieDetailsOnUI();
+
+        playerFragment =
+                (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_fragment);
+
+        playerFragment.initialize(YouTubeKey, this);
     }
 
     private void populateMovieDetailsOnUI() {
@@ -52,5 +68,27 @@ public class DetailsActivity extends AppCompatActivity {
             Toast.makeText(this, "No data to present.", Toast.LENGTH_LONG).show();
             finish();
         }
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        mPlayer = youTubePlayer;
+
+        if (!b) {
+            // TODO: Add GridLayout to show the video list and onClick to load the movie via the key.
+            // Example for loading video list
+            List videoList = new ArrayList();
+            videoList.add("Q0CbN8sfihY");
+            videoList.add("-py2awmME8s");
+            videoList.add("WcIfbdfZRDQ");
+            mPlayer.cueVideos(videoList);
+        } else {
+            mPlayer.play();
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        mPlayer = null;
     }
 }
